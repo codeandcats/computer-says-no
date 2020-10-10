d9992d376689238f5bcae5171ad80248288723f4
Benji <codeandcats@gmail.com>
2020-10-10 14:22:21 +1100

    feat: various improvements
      - Error message is now completely optional (making it manditatory
        doesn't scale well for i18n)
    
      - Error constructor is much more flexible now, can accept a string
        (for a message), an object (for fields), or a function returning
        either a string or object.
    
      - Added isError function for testing if value is a computer-says-no
        Error

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,58 +1,96 @@
 import * as ta from 'type-assertions';
 
-import { defineError } from '.';
+import { defineError, isError } from '.';
 
-describe('defineException', () => {
-  it('defines an exception with the given code', () => {
-    const NotFoundError = defineError('NOT_FOUND', () => 'Not found');
-
+describe('defineError', () => {
+  it('defines an error with the given code', () => {
+    const NotFoundError = defineError('NOT_FOUND');
     expect(NotFoundError.code).toEqual('NOT_FOUND');
   });
 
-  it('allows you to create an exception from the definition', () => {
-    const NotFoundError = defineError('NOT_FOUND', () => 'Not found');
+  it('allows you to create an error from the definition', () => {
+    const NotFoundError = defineError('NOT_FOUND');
     const error = NotFoundError();
-
     expect(error.code).toEqual('NOT_FOUND');
-    expect(error.message).toEqual('Not found');
   });
 
-  it('allows you to create an exception from the definition using new', () => {
-    const NotFoundError = defineError('NOT_FOUND', () => 'Not found');
+  it('allows you to create an error using new keyowrd', () => {
+    const NotFoundError = defineError('NOT_FOUND');
     const error = new NotFoundError();
-
     expect(error.code).toEqual('NOT_FOUND');
-    expect(error.message).toEqual('Not found');
   });
 
-  it('allows you to create an exception from the definition that takes parameters', () => {
+  it('allows you to define and create errors with messages', () => {
+    // There's many ways to create an error with a message property
+    // Note that the message property is optional, as it doesn't make a lot of sense for i18n
+    const NotFoundError1 = defineError('NOT_FOUND', 'Not found');
+    const NotFoundError2 = defineError('NOT_FOUND', () => 'Not found');
+    const NotFoundError3 = defineError('NOT_FOUND', { message: 'Not found' });
+    const NotFoundError4 = defineError('NOT_FOUND', () => ({ message: 'Not found' }));
+
+    const performChecks = <T extends { code: string, message: string }>(error: T) => {
+      expect(error.code).toEqual('NOT_FOUND');
+      expect(error.message).toEqual('Not found');
+    };
+
+    performChecks(NotFoundError1());
+    performChecks(new NotFoundError1());
+
+    performChecks(NotFoundError2());
+    performChecks(new NotFoundError2());
+
+    performChecks(NotFoundError3());
+    performChecks(new NotFoundError3());
+
+    performChecks(NotFoundError4());
+    performChecks(new NotFoundError4());
+  });
+
+  it('allows you to create an error from a definition that takes parameters', () => {
     const ValueRequiredError = defineError(
       'VALUE_REQUIRED',
       (fieldName: string, fieldDisplayName: string) => ({
         message: `${fieldDisplayName} is required`,
         fieldName,
         fieldDisplayName,
-      }),
+      })
     );
 
-    const error = ValueRequiredError('emailAddress', 'E-mail');
-    expect(error.code).toEqual('VALUE_REQUIRED');
-    expect(error.message).toEqual('E-mail is required');
-    expect(error.fieldName).toEqual('emailAddress');
-    expect(error.fieldDisplayName).toEqual('E-mail');
+    type ExpectedType = {
+      code: 'VALUE_REQUIRED',
+      message: string,
+      fieldName: string,
+      fieldDisplayName: string
+    };
+
+    const error1 = ValueRequiredError('emailAddress', 'E-mail');
+    expect(error1.code).toEqual('VALUE_REQUIRED');
+    expect(error1.message).toEqual('E-mail is required');
+    expect(error1.fieldName).toEqual('emailAddress');
+    expect(error1.fieldDisplayName).toEqual('E-mail');
+    ta.assert<ta.Equal<typeof error1, ExpectedType>>();
+
+    // Perform same test with `new` keyword
+    const error2 = new ValueRequiredError('emailAddress', 'E-mail');
+    expect(error2.code).toEqual('VALUE_REQUIRED');
+    expect(error2.message).toEqual('E-mail is required');
+    expect(error2.fieldName).toEqual('emailAddress');
+    expect(error2.fieldDisplayName).toEqual('E-mail');
+    ta.assert<ta.Equal<typeof error2, ExpectedType>>();
   });
 
   it('correctly identifies the type of an error', () => {
     const NotFoundError = defineError('NOT_FOUND', () => 'Not found');
-    const AccessDeniedError = defineError(
-      'ACCESS_DENIED',
-      () => 'Access denied',
-    );
-
+    const AccessDeniedError = defineError('ACCESS_DENIED', 'Access denied');
+    
     const error = AccessDeniedError();
-
     expect(NotFoundError.is(error)).toEqual(false);
     expect(AccessDeniedError.is(error)).toEqual(true);
+
+    // Perform test with `new` keyword
+    const error2 = new NotFoundError();
+    expect(NotFoundError.is(error2)).toEqual(true);
+    expect(AccessDeniedError.is(error2)).toEqual(false);
   });
 
   it('correctly identifies the type of an error even after its been serialized and deserialized', () => {
@@ -72,13 +110,11 @@ describe('defineException', () => {
 
   // eslint-disable-next-line jest/expect-expect
   it('correctly infers properties available on an error based on its asserted type', () => {
-    const UserNotFoundError = defineError(
-      'USER_NOT_FOUND',
-      (username: string) => ({
-        message: 'User not found',
-        username,
-      }),
-    );
+    const createUserNotFound = (username: string) => ({
+      message: 'User not found',
+      username,
+    });
+    const UserNotFoundError = defineError('USER_NOT_FOUND', createUserNotFound);
 
     const someUnknownError: unknown = undefined;
 
@@ -92,11 +128,78 @@ describe('defineException', () => {
         message: string;
         username: string;
       };
-      ta.assert<ta.Equal<typeof someUnknownError, ExpectedType>>();
+      type ActualType = typeof someUnknownError;
+      ta.assert<ta.Equal<ActualType, ExpectedType>>();
 
       ta.assert<ta.Not<ta.Equal<typeof someUnknownError, unknown>>>();
     } else {
       ta.assert<ta.Equal<typeof someUnknownError, unknown>>();
     }
+  });
+
+  it('can handle thrown errors', () => {
+    const InvalidEmailAddressError = defineError('INVALID_EMAIL_ADDRESS', (emailAddress: string) => ({
+      emailAddress,
+      message: `Invalid E-mail "${emailAddress}"`,
+    }));
+
+    const checkError = (error: any) => {
+      expect(error).toBeDefined();
+      expect(InvalidEmailAddressError.is(error)).toEqual(true);
+      expect(error.code).toEqual('INVALID_EMAIL_ADDRESS');
+      expect(error.emailAddress).toEqual('foo');
+      expect(error.message).toEqual('Invalid E-mail "foo"');
+    }
+
+    let error1: any;
+    try {
+      throw InvalidEmailAddressError('foo');
+    } catch (err) {
+      error1 = err;
+    }
+    checkError(error1);
+
+    // Perform same test with `new` keyword
+    let error2: any;
+    try {
+      throw new InvalidEmailAddressError('foo');
+    } catch (err) {
+      error2 = err;
+    }
+    checkError(error2);
+  });
+});
+
+describe('isError', () => {
+  it('returns true for any error created from computer-says-no', () => {
+    const NopeError = defineError('NOPE');
+    // We'lll stringify and parse it as an extra test
+    const error = JSON.parse(JSON.stringify(NopeError()));
+    expect(isError(error)).toEqual(true);
+
+    // Same test using `new` keyword
+    const error2 = JSON.parse(JSON.stringify(new NopeError()));
+    expect(isError(error2)).toEqual(true);
+  });
+
+  it('returns false for anything else', () => {
+    expect(isError(true)).toEqual(false);
+    expect(isError(false)).toEqual(false);
+    expect(isError(Error)).toEqual(false);
+    expect(isError(new Error("I'm a standard JS Error"))).toEqual(false);
+  });
+
+  it('returns whether an error of a specific type', () => {
+    const FooError = defineError('FOO');
+    const BarError = defineError('BAR');
+
+    const error = JSON.parse(JSON.stringify(FooError()));
+    expect(isError(error, FooError)).toEqual(true);
+    expect(isError(error, BarError)).toEqual(false);
+
+    // Same test using `new` keyword
+    const error2 = JSON.parse(JSON.stringify(new FooError()));
+    expect(isError(error2, FooError)).toEqual(true);
+    expect(isError(error2, BarError)).toEqual(false);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,155 @@
+const CFN_ERROR_IDENTIFIER = '$$cfn';
+
 type Dictionary = { [K: string]: unknown };
 
-type BaseErrorBody = { message: string } | string;
-
-type BaseError<
-  TCode extends string,
-  TBody extends BaseErrorBody
-> = {
-  code: TCode;
-} & (
-  TBody extends string
-    ? { message: TBody }
-    : TBody
+type SerializableValue = (
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | SerializableValue[]
+  | { [K: string]: SerializableValue }
 );
 
-interface BaseErrorClass<
+export type BaseErrorBody = Record<string, SerializableValue> | string;
+
+type ErrorConstructor = BaseErrorBody | ((...args: any[]) => BaseErrorBody) | undefined;
+
+type GetBaseErrorBody<TConstructor extends ErrorConstructor> = (
+  TConstructor extends (...args: any[]) => BaseErrorBody
+    ? (
+      ReturnType<TConstructor> extends string
+        ? { message: ReturnType<TConstructor> }
+        : ReturnType<TConstructor>
+    )
+    : (
+      TConstructor extends string
+        ? { message: TConstructor }
+        : (
+          TConstructor extends undefined
+            ? Record<string, unknown>
+            : TConstructor
+        )
+    )
+);
+
+export type BaseError<
   TCode extends string,
-  TArgs extends unknown[],
-  TBody extends BaseErrorBody
-> {
-  (...args: TArgs): BaseError<TCode, TBody>;
-  new (...args: TArgs): BaseError<TCode, TBody>;
-  code: TCode;
-  is(err: unknown): err is BaseError<TCode, TBody>;
+  TConstructor extends ErrorConstructor
+> = { code: TCode } & GetBaseErrorBody<TConstructor>;
+
+type BaseErrorDefinition<
+  TCode extends string,
+  TConstructor extends ErrorConstructor
+> = (
+  {
+    code: TCode
+    is(err: unknown): err is BaseError<TCode, TConstructor>;
+  } & (
+    TConstructor extends ((...args: any[]) => BaseErrorBody)
+      ? {
+        (...args: Parameters<TConstructor>): BaseError<TCode, TConstructor>;
+        new (...args: Parameters<TConstructor>): BaseError<TCode, TConstructor>;
+      }
+      : {
+        (): BaseError<TCode, TConstructor>;
+        new (): BaseError<TCode, TConstructor>;
+      }
+  )
+);
+
+function getErrorBody<
+  TCode extends string,
+  TConstructor extends (...args: any[]) => BaseErrorBody,
+>(code: TCode, create: TConstructor, args: Parameters<TConstructor>): BaseError<TCode, TConstructor>;
+
+function getErrorBody<
+  TCode extends string,
+  TConstructor extends BaseErrorBody
+>(code: TCode, create: TConstructor): BaseError<TCode, TConstructor>;
+
+function getErrorBody<
+  TCode extends string,
+  TConstructor extends ErrorConstructor
+>(
+  code: TCode,
+  create: TConstructor,
+  args?: any
+) {
+  const bodyRaw = typeof create === 'function' ? create(...args) : create;
+
+  const body = {
+    ...(typeof bodyRaw === 'string'
+      ? { message: bodyRaw }
+      : bodyRaw as Exclude<GetBaseErrorBody<TConstructor>, string>),
+    code,
+  };
+
+  return body;
 }
 
 export function defineError<
   TCode extends string,
-  TArgs extends unknown[],
-  TBody extends BaseErrorBody
+  TConstructor extends ErrorConstructor
 >(
   code: TCode,
-  create: (...args: TArgs) => TBody
+  create?: TConstructor,
 ) {
-  function ErrorConstructor(this: BaseError<TCode, TBody>, ...args: TArgs) {
-    const body = create(...args);
-    const bodyNormalised = { ...(typeof body === 'string' ? { message: body } : body as Exclude<TBody, string>), code };
+  type Args = TConstructor extends () => BaseErrorBody ? Parameters<TConstructor> : never;
+
+  function ErrorDefinition(
+    this: BaseError<TCode, TConstructor>,
+    ...args: Args
+  ) {
+    const body = typeof create === 'function'
+      ? getErrorBody(code, create as (...a: any[]) => BaseErrorBody, args)
+      : getErrorBody(code, create as BaseErrorBody);
+
+    (body as any)[CFN_ERROR_IDENTIFIER] = 1;
 
     if (new.target) {
-      Object.keys(bodyNormalised).forEach((key) => {
-        (this as Dictionary)[key] = (bodyNormalised as Dictionary)[key];
+      Object.keys(body).forEach((key) => {
+        (this as Dictionary)[key] = (body as Dictionary)[key];
       });
       return this;
     }
 
-    return bodyNormalised;
+    return body;
   }
-  ErrorConstructor.code = code;
-  ErrorConstructor.is = (err: unknown): err is BaseError<TCode, TBody> => {
-    const baseError = err as BaseError<TCode, TBody>;
+  ErrorDefinition.code = code;
+  ErrorDefinition.is = (err: unknown): err is BaseError<TCode, TConstructor> => {
+    const baseError = err as BaseError<TCode, TConstructor>;
     return baseError && baseError.code === code;
   };
 
-  Object.defineProperty(ErrorConstructor, 'name', {
+  Object.defineProperty(ErrorDefinition, 'name', {
     value: 'Foo',
     writable: false
   });
 
-  return ErrorConstructor as BaseErrorClass<TCode, TArgs, TBody>;
+  return ErrorDefinition as unknown as BaseErrorDefinition<TCode, TConstructor>;
+}
+
+export function isError(value: unknown): value is { code: string; message?: string };
+export function isError<
+  TCode extends string,
+  TConstructor extends ErrorConstructor,
+  TErrorDefinition extends BaseErrorDefinition<TCode, TConstructor>
+>(
+  value: unknown,
+  definition: TErrorDefinition
+): value is BaseError<TCode, TConstructor>;
+
+export function isError(value: unknown, definition?: BaseErrorDefinition<any, any>): boolean {
+  const isCsnError = value && (value as any)[CFN_ERROR_IDENTIFIER];
+  if (!isCsnError) {
+    return false;
+  }
+
+  if (!definition) {
+    return true;
+  }
+
+  return definition.is(value);
 }
